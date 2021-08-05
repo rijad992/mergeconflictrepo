8d2668c92b79eabb4d0b1d80d7a037bd53acc0be
Rijad Lendo <rijad.lendo@rubicon.ba>
2021-08-05 10:47:15 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const var1 = 1;
+const var2 = 2;
+const var3 = 3;
+const var5 = 6;
+
+function func1(){
+    console.log()
+}
+
+function func2(){
+    console.log()
+}
+
+function func3(){
+    console.log()
+}
+
+function func4(){
+    console.log()
+}
